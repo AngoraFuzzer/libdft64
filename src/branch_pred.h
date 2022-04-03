7e3f668226dce027e8be787873dda9c3a62c9b5e
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2022-04-03 14:44:11 +0300

    unlikely produced wrong results on Win32, set as nop for now.

--- src/branch_pred.h
+++ src/branch_pred.h
@@ -34,7 +34,12 @@
 #define __BRANCH_PRED_H__
 
 /* compiler directives for branch prediction */
+#ifdef _WIN32
+#define likely(x)       (x)
+#define unlikely(x)     (x)
+#else
 #define likely(x)       __builtin_expect(!!(x), true)
 #define unlikely(x)     __builtin_expect(!!(x), false)
+#endif // _WIN32
 
 #endif /* __BRANCH_PRED_H__ */
